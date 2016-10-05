73ffb5316ee8dd6a0eeb220ef6f3fa764f3b1c55
George Stagas <gstagas@gmail.com>
2016-10-05 15:01:45 +0300

    add bank triggers

--- algorave.js
+++ algorave.js
@@ -1,4 +1,51 @@
 
+function PadKey(char) {
+  this.el = document.createElement('div');
+  this.el.className = 'key key-' + char;
+  this.el.textContent = char;
+  this.charCode = char.charCodeAt(0);
+  this.name = char;
+  this.turnOff();
+}
+
+PadKey.prototype.turnOn = function() {
+  this.el.classList.add('active');
+  this.active = true;
+};
+
+PadKey.prototype.turnOff = function() {
+  this.el.classList.remove('active');
+  this.active = false;
+};
+
+PadKey.prototype.toggle = function() {
+  if (this.active) {
+    this.turnOff();
+  } else {
+    this.turnOn();
+  }
+};
+
+function BankKey(char) {
+  PadKey.call(this, char);
+}
+
+BankKey.prototype.__proto__ = PadKey.prototype;
+
+function SoundKey(char) {
+  PadKey.call(this, char);
+  this.setBank('1');
+}
+
+SoundKey.prototype.__proto__ = PadKey.prototype;
+
+SoundKey.prototype.setBank = function(bank) {
+  this.el.classList.remove('bank-' + this.bank);
+  this.bank = bank;
+  this.el.classList.add('bank-' + this.bank);
+};
+
+
 var keyboard = [
   '1234567890'.split(''),
   'qwertyuiop'.split(''),
@@ -6,7 +53,8 @@ var keyboard = [
   'zxcvbnm'.split(''),
 ];
 
-var keys = {};
+var sounds = {};
+var banks = {};
 
 var keyboardElement = document.createElement('div');
 keyboardElement.className = 'keyboard';
@@ -16,12 +64,17 @@ for (var i = 0; i < keyboard.length; i++) {
   var rowElement = document.createElement('div');
   rowElement.className = 'row row-' + i;
   for (var k = 0; k < row.length; k++) {
-    var key = row[k];
-    var keyElement = document.createElement('div');
-    keyElement.className = 'key';
-    keyElement.textContent = key;
-    keys[key.charCodeAt(0)] = keyElement;
-    rowElement.appendChild(keyElement);
+    var char = row[k];
+    var key;
+    if (i === 0) {
+      key = new BankKey(char);
+      banks[key.charCode] = key;
+      rowElement.appendChild(key.el);
+    } else {
+      key = new SoundKey(char);
+      sounds[key.charCode] = key;
+      rowElement.appendChild(key.el);
+    }
   }
   keyboardElement.appendChild(rowElement);
 }
@@ -29,11 +82,32 @@ for (var i = 0; i < keyboard.length; i++) {
 document.body.appendChild(keyboardElement);
 
 document.body.onkeydown = e => {
-  var key = keys[e.key.charCodeAt(0)];
-  if (key == null) return;
-  if (key.classList.contains('active')) {
-    key.classList.remove('active');
+  var charCode = e.key.charCodeAt(0);
+  var key = sounds[charCode];
+  if (key == null) {
+    key = banks[charCode];
+    if (key == null) return;
+    else {
+      state.activeBank.turnOff();
+      state.activeBank = key;
+      state.triggerBank = true;
+      key.turnOn();
+    }
   } else {
-    key.classList.add('active');
+    if (state.triggerBank) {
+      key.setBank(state.activeBank.name);
+      state.activeBank.turnOff();
+      state.triggerBank = false;
+      key.turnOn();
+    } else {
+      key.toggle();
+    }
   }
 };
+
+var state = {
+  activeBank: banks[49],
+  triggerBank: false
+};
+
+state.activeBank.turnOn();
